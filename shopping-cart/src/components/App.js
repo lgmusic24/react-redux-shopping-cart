d3bc449af7ed1e9dd9f893e4f8fc6bf2af4dcbbc
lgmusic24 <lgmusic24@gmail.com>
2017-05-31 16:05:08 -0400

    updated changes

---
+++ shopping-cart/src/components/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Cart from '../containers/Cart'
+import '../stylesheets/App.css';
+
+
+class App extends Component {
+  render() {
+    return (
+      <Cart />
+    );
+  }
+}
+
+
+export default App;
